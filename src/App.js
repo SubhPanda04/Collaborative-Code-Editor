9b0abe95301c05d0e1ba1f3520cdc36478edd6b4
Tejash <tejashmaru2003@gmail.com>
2025-01-27 02:16:51 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{ useEffect } from 'react';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Home } from './container';
+import { auth } from './config/firebase.config';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userCred) => {
+      if(userCred) {
+        console.log(userCred?.providerData[0]);
+      }
+      else {
+        navigate("/home/auth", { replace: true });
+      }
+    });
+  },[]);
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+
+        {/*If the route is not matching */}
+        <Route path="*" element={<Navigate to={"/home"} />}/>
+      </Routes>
     </div>
   )
 }
