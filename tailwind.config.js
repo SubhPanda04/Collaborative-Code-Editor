21146959cd6293593407a384af8a0a886bad18ff
Tejash <tejashmaru2003@gmail.com>
2025-01-27 02:17:50 +0530

    updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#131417",
+        secondary: "#1E1F26",
+        primaryText: "#868CA0",
+        text555: "#555",
+    },
   },
+},
   plugins: [],
-}
+};
 
