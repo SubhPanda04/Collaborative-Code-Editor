dea4192cd078f702b2207519851516e02ffdde93
Tejash <tejashmaru2003@gmail.com>
2025-02-12 20:14:06 +0530

    useEffect changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React,{ useEffect } from 'react';
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import { Home } from './container';
-import { auth } from './config/firebase.config';
+import { auth,db } from './config/firebase.config';
+import { doc, getDoc } from  "firebase/firestore"
 
 const App = () => {
   const navigate = useNavigate();
 
-  const unsubscribe = auth.onAuthStateChanged((userCred) => {
-    if(userCred) {
-      console.log(userCred?.providerData[0]);
-    }
-    else {
-      navigate("/home/auth", { replace: true });
-    }
-  });
+
 
   useEffect(() => {
-    unsubscribe();
-  });
+    const unsubscribe = auth.onAuthStateChanged(async (userCred) => {
+      if (userCred) {
+        const userDoc = await getDoc(doc(db, "users", userCred.uid));
+        if (!userDoc.exists()) {
+          console.log("User data not found in Firestore. Redirecting to sign-up.");
+          navigate("/home/auth", { replace: true });
+        } else {
+          console.log("User data:", userDoc.data());
+        }
+      } else {
+        navigate("/home/auth", { replace: true });
+      }
+    });
+  
+    return () => unsubscribe();
+  }, [navigate]);
   return (
     <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
       <Routes>
