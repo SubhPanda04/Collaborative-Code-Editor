dc76ead13e45f6da6662c938a195d096d7ee5e64
Tejash <tejashmaru2003@gmail.com>
2025-02-14 22:54:17 +0530

    session storage for firebase

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,5 @@
 import { getApps, getApp, initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { getAuth, setPersistence,browserSessionPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,5 +15,14 @@ const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Set authentication persistence to SESSION-based
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    console.log("Session-based authentication enabled.");
+  })
+  .catch((error) => {
+    console.error("Error setting authentication persistence:", error);
+  });
 
 export {app, auth, db};
