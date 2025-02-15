2ceef8448d16a7f7b4d2783c1ef2b101420d1dc6
SubhPanda04 <subhasispanda1256700@gmail.com>
2025-02-15 09:06:07 +0530

    Updated export

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
-export {default as UserAuthInput } from "./UserAuthInput"
+export { default as UserAuthInput } from "./UserAuthInput";
+export { default as Header } from "./Header";
+export { default as Sidebar } from "./Sidebar";
+export { default as Editor } from "./Editor";
