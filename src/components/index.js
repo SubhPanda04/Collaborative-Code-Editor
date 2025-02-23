48a79c41be9f77f9481b48008aa6b469c11ebf6d
SubhPanda04 <subhasispanda1256700@gmail.com>
2025-02-24 01:44:54 +0530

    Deleted files

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UserAuthInput } from "./UserAuthInput";
-export { default as Header } from "./Header";
-export { default as Sidebar } from "./Sidebar";
-export { default as Editor } from "./Editor";
