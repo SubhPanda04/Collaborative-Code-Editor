62e09a9df09e3dc152fe1b531b58c553ad80d5e9
SubhPanda04 <subhasispanda1256700@gmail.com>
2025-03-22 01:04:33 +0530

    Deleting all the files and shifting to Frontend directory

--- src/redux/hooks.jsx
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-// Export hooks without TypeScript annotations
-export const useAppDispatch = useDispatch;
-export const useAppSelector = useSelector; 
